dfb8bb1144c07bdca23627104f11e06188ee8171
kaueriki <kaueriki@gmail.com>
2024-05-15 15:42:30 -0300

    Restruturação do portfólio

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
-from flask import Flask, request, render_template,  url_for
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./static", template_folder="./templates")
 
 
 
 
 @app.route("/")
 def home():
-    titulo = "Home"
-    return render_template ("home.html", title = titulo)
+    titulo = "Portfólio"
+    return render_template ("index.html", title = titulo)
 
-@app.route("/info")
-def info():
-    titulo = "Informações Educacionais"
-    return render_template ("info.html", title = titulo)
-
-@app.route("/hobbies")
-def hobbies():
-    titulo = "Hobbies"
-    return render_template ("hobbies.html", title = titulo)
-
-@app.route("/redes")
-def redes():
-    titulo = "Redes Sociais"
-    return render_template ("redes.html", title = titulo)
 
 if __name__ == '__main__':
     app.run(debug=True)
